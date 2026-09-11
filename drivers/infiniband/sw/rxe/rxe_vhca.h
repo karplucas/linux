27ce7d4536f816e0f0ae9faf0af343bdb5238bca
Lucas Karpinski <lkarpinski@nvidia.com>
2026-09-10 20:08:00 -0700

    RDMA/rxe: define responder resource image records
    Define fixed little-endian fields for responder resources in the RXE vHCA
    image. Keep runtime pointers and native structure layout out of the stream.
    
    Signed-off-by: Lucas Karpinski <lkarpinski@nvidia.com>

--- drivers/infiniband/sw/rxe/rxe_vhca.h
+++ drivers/infiniband/sw/rxe/rxe_vhca.h
@@ -2,6 +2,7 @@
 #ifndef RXE_VHCA_H
 #define RXE_VHCA_H
 
+#include <linux/bits.h>
 #include <linux/types.h>
 
 #define RXE_VHCA_IMAGE_MAGIC	0x56455852
@@ -35,6 +36,55 @@ struct rxe_vhca_qp_header {
 	__le32 resp_resource_count;
 };
 
+enum rxe_vhca_resp_resource_type {
+	RXE_VHCA_RESP_RESOURCE_READ = 1,
+	RXE_VHCA_RESP_RESOURCE_ATOMIC,
+	RXE_VHCA_RESP_RESOURCE_ATOMIC_WRITE,
+	RXE_VHCA_RESP_RESOURCE_FLUSH,
+};
+
+enum rxe_vhca_resp_resource_state {
+	RXE_VHCA_RESP_RESOURCE_NEXT,
+	RXE_VHCA_RESP_RESOURCE_NEW,
+	RXE_VHCA_RESP_RESOURCE_REPLAY,
+};
+
+#define RXE_VHCA_RESP_RESOURCE_F_REPLAY	BIT(0)
+
+struct rxe_vhca_resp_resource {
+	__le32 slot;
+	__le32 type;
+	__le32 flags;
+	__le32 state;
+	__le32 first_psn;
+	__le32 last_psn;
+	__le32 cur_psn;
+	__le32 reserved;
+	union {
+		struct {
+			__le64 va_org;
+			__le64 va;
+			__le32 rkey;
+			__le32 length;
+			__le32 resid;
+			__le32 reserved;
+		} read;
+		struct {
+			__le64 orig_val;
+			__le64 reserved[3];
+		} atomic;
+		struct {
+			__le64 va;
+			__le32 length;
+			u8 type;
+			u8 level;
+			__le16 reserved;
+			__le64 reserved2[2];
+		} flush;
+		u8 raw[32];
+	} data;
+};
+
 struct rxe_vhca_writer {
 	u8 *data;
 	size_t capacity;
@@ -54,6 +104,8 @@ struct rxe_vhca_record {
 	u32 flags;
 };
 
+struct resp_res;
+
 int rxe_vhca_writer_init(struct rxe_vhca_writer *writer, void *data,
 			 size_t capacity);
 int rxe_vhca_write_record(struct rxe_vhca_writer *writer, u32 type,
@@ -62,5 +114,9 @@ int rxe_vhca_reader_init(struct rxe_vhca_reader *reader, const void *data,
 			 size_t length);
 int rxe_vhca_read_record(struct rxe_vhca_reader *reader,
 			 struct rxe_vhca_record *record);
+int rxe_vhca_encode_resp_resource(struct rxe_vhca_resp_resource *record,
+				  u32 slot, const struct resp_res *resource);
+int rxe_vhca_decode_resp_resource(const struct rxe_vhca_resp_resource *record,
+				  u32 slots, struct resp_res *resource);
 
 #endif /* RXE_VHCA_H */
